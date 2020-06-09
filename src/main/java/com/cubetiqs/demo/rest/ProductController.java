c71b0d90a7ee6a7a35f84816efe669fc47dbf00b
Sambo Chea <sombochea@cubetiqs.com>
2020-06-09 19:57:21 +0700

    Updated product controller

--- src/main/java/com/cubetiqs/demo/rest/ProductController.java
+++ src/main/java/com/cubetiqs/demo/rest/ProductController.java
@@ -5,7 +5,6 @@ import com.cubetiqs.demo.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.math.BigDecimal;
 import java.util.List;
 
 @RestController
