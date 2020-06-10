f9caf17a1853f96f86949aa05354507f90b69119
Sambo Chea <sombochea@cubetiqs.com>
2020-06-10 17:58:31 +0700

    Removed classes

--- src/main/java/com/cubetiqs/demo/repository/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package com.cubetiqs.demo.repository;
-
-import com.cubetiqs.demo.domain.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-
-}
